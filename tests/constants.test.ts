3997e7e40d117d83b5c952de233dc5da34a30a52
bram <bram@brams.dev>
2026-04-08 15:18:27 +0200

    fix: regex

--- tests/constants.test.ts
+++ tests/constants.test.ts
@@ -5,7 +5,7 @@ describe('RELEASE_HEADING_REGEX', () => {
 	test('unreleased', () => {
 		const matches = RELEASE_HEADING_REGEX.exec('## [Unreleased]');
 
-		expect(matches).toEqual([
+		expect([...(matches ?? [])]).toEqual([
 			'## [Unreleased]',
 			'Unreleased',
 			undefined,
@@ -16,6 +16,46 @@ describe('RELEASE_HEADING_REGEX', () => {
 	test('release', () => {
 		const matches = RELEASE_HEADING_REGEX.exec('## [1.0.0]');
 
-		expect(matches).toEqual(['## [1.0.0]', '1.0.0', undefined, undefined]);
+		expect([...(matches ?? [])]).toEqual([
+			'## [1.0.0]',
+			'1.0.0',
+			undefined,
+			undefined,
+		]);
+	});
+
+	test('release with date', () => {
+		const matches = RELEASE_HEADING_REGEX.exec('## [1.0.0] - 2026-04-08');
+
+		expect([...(matches ?? [])]).toEqual([
+			'## [1.0.0]',
+			'1.0.0',
+			'2026-04-08',
+			undefined,
+		]);
+	});
+
+	test('release with label', () => {
+		const matches = RELEASE_HEADING_REGEX.exec('## [1.0.0] - (Sprint x)');
+
+		expect([...(matches ?? [])]).toEqual([
+			'## [1.0.0]',
+			'1.0.0',
+			undefined,
+			'Sprint x',
+		]);
+	});
+
+	test('release with date and label', () => {
+		const matches = RELEASE_HEADING_REGEX.exec(
+			'## [1.0.0] - 2026-04-08 (Sprint x)',
+		);
+
+		expect([...(matches ?? [])]).toEqual([
+			'## [1.0.0]',
+			'1.0.0',
+			'2026-04-08',
+			'Sprint x',
+		]);
 	});
 });
